da79a38c2a59927a47c5d5386ba8a26afed4d4c8
Théo.P <theopichonneau@orange.fr>
2023-11-28 16:13:11 +0100

    Ajout de la barre de navigation.

---
+++ Portfolio/src/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+      <div class="nav-item">Item 1</div>
+      <div class="nav-item">Item 2</div>
+      <div class="nav-item">Item 3</div>
+      <div class="nav-item">Item 4</div>
+    </nav>
+  </template>
+  
+  <style scoped>
+    .nav-item {
+      padding: 10px;
+      border: 1px solid #ddd;
+      margin: 5px;
+    }
+  </style>
