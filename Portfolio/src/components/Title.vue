9d3ba7184590cdb9afee06374061413ecf162da3
Théo.P <theopichonneau@orange.fr>
2023-11-28 16:20:50 +0100

    Barre navigation a configurer et Titre/sous-titre

---
+++ Portfolio/src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>{{ title }}</h2>
+      <h3>{{ subtitle }}</h3>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: String,
+      subtitle: String,
+    },
+  };
+  </script>
